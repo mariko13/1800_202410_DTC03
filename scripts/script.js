db381de63bf2263993c89185622ee85c5b249306
Homayoun Khoshi <Khoshihomayoun@gmail.com>
2024-03-18 14:50:18 -0700

    Implement login and logout functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
 function navigateToPage(pageUrl) {  
     window.location.href = pageUrl;
 }
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
