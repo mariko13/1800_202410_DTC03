d09c67b5ea62fa304c7e31ae292cbdc48a6c0e3e
Homayoun Khoshi <Khoshihomayoun@gmail.com>
2024-03-19 12:10:18 -0700

    implement personal info

--- scripts/home.js
+++ scripts/home.js
@@ -1,19 +1,55 @@
-function insertNameFromFirestore() {
-  // Check if the user is logged in:
-  firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-          console.log(user.uid); // Let's know who the logged-in user is by logging their UID
-          currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
-          currentUser.get().then(userDoc => {
-              // Get the user name
-              let userName = userDoc.data().name;
-              console.log(userName);
-              //$("#name-goes-here").text(userName); // jQuery
-              document.getElementById("name-goes-here").innerText = userName;
-          })
-      } else {
-          console.log("No user is logged in."); // Log a message when no user is logged in
-      }
-  })
+var currentUser;               //points to the document of the user who is logged in
+function populateUserInfo() {
+            firebase.auth().onAuthStateChanged(user => {
+                // Check if user is signed in:
+                if (user) {
+
+                    //go to the correct user document by referencing to the user uid
+                    currentUser = db.collection("users").doc(user.uid)
+                    //get the document for current user.
+                    currentUser.get()
+                        .then(userDoc => {
+                            //get the data fields of the user
+                            let userAge = userDoc.data().age;
+                            let userGender = userDoc.data().gender;
+                            let userLocation = userDoc.data().location;
+
+                            //if the data fields are not empty, then write them in to the form.
+                            if (userAge != null) {
+                                document.getElementById("age").value = userAge;
+                            }
+                            if (userGender != null) {
+                                document.getElementById("genderselect").value = userGender;
+                            }
+                            if (userLocation != null) {
+                                document.getElementById("location").value = userLocation;
+                            }
+                        })
+                } else {
+                    // No user is signed in.
+                    console.log ("No user is signed in");
+                }
+            });
+        }
+
+//call the function to run it 
+populateUserInfo();
+
+function saveUserInfo() {
+    //enter code here
+
+    //a) get user entered values
+    userAge = document.getElementById('age').value;       
+    userGender = document.getElementById('genderselect').value;     
+    userLocation = document.getElementById('location').value;
+    //b) update user's document in Firestore
+    currentUser.update({
+        age: userAge,
+        gender: userGender,
+        location: userLocation
+    })
+    .then(() => {
+        console.log("Document successfully updated!");
+    })
+    //c) disable edit 
 }
-insertNameFromFirestore();
--- scripts/personalinfo.js
+++ scripts/personalinfo.js
@@ -1,29 +1,24 @@
 // Purpose: This script is used to add the user's personal information to the firestore database.
-let age_amount = prompt("Please enter your age"); 
+let age_amount = prompt("Please enter your age");
 let gender_type = prompt("Please enter your gender");
 let location_place = prompt("Please enter your location");
+console.log(age_amount);
 
-var uiConfig = {
-  callbacks: {
-writepersonalinfo: function(authResult, redirectUrl) {
-      var user = authResult.user;                            // get the user object from the Firebase authentication database
-             
-          db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
-                 name: user.displayName,                    //"users" collection
-                 email: user.email,
-                 age: age_amount,       
-                 gender: gender_type,  
-                 location: location_place,                //with authenticated user's ID (user.uid)
-                 country: "Canada",                      //optional default profile info      
-                 school: "BCIT"                          //optional default profile info
-          }).then(function () {
-                 console.log("New user added to firestore");
-                 window.location.assign("home.html");       //re-direct to personalinfo.html after signup
-          }).catch(function (error) {
-                 console.log("Error adding new user: " + error);
-          });
-        }
-  }
-}
-      
+// get the user object from the Firebase authentication database
+ var user = authResult.user;
+db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
+  name: user.displayName,                    //"users" collection
+  email: user.email,
+  age: age_amount,
+  gender: gender_type,
+  location: location_place,                //with authenticated user's ID (user.uid)
+  country: "Canada",                      //optional default profile info      
+  school: "BCIT"                          //optional default profile info
+}).then(function () {
+  console.log("New user added to firestore");
+  window.location.assign("home.html");       //re-direct to personalinfo.html after signup
+}).catch(function (error) {
+  console.log("Error adding new user: " + error);
+});
+
 
