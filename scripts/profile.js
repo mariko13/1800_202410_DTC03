b67f949581ce797350e2dbf80f3c080b2b965cec
Homayoun Khoshi <Khoshihomayoun@gmail.com>
2024-03-24 18:27:21 -0700

    Implement js for profile page

---
+++ scripts/profile.js
@@ -0,0 +1,28 @@
+var currentUser;               //points to the document of the user who is logged in
+
+function insertNameFromFirestore() {
+    // Check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+            currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
+            currentUser.get().then(userDoc => {
+                // Get the user name
+                let userName = userDoc.data().name;
+                let userAge = userDoc.data().age;
+                let userGender = userDoc.data().gender;
+                let userLocation = userDoc.data().location;
+                console.log(userName);
+                //$("#name-goes-here").text(userName); // jQuery
+                document.getElementById("name-goes-here").innerText = userName;
+                document.getElementById("age-goes-here").innerText = userAge;
+                document.getElementById("gender-goes-here").innerText = userGender;
+                document.getElementById("location-goes-here").innerText = userLocation;
+            })
+        } else {
+            console.log("No user is logged in."); // Log a message when no user is logged in
+        }
+    })
+}
+
+insertNameFromFirestore(); 
