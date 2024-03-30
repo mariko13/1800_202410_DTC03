b7f7cbab553632800603307034519b2c93d68e9e
HWEEDONG YOO <yoojon675@gmail.com>
2024-03-30 14:00:46 -0700

    Add scrolling event listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,21 @@
-function navigateToPage(pageUrl) {  
-    window.location.href = pageUrl;
+function navigateToPage(pageUrl) {
+  window.location.href = pageUrl;
 }
 function logout() {
-    firebase.auth().signOut().then(() => {
-        // Sign-out successful.
-        console.log("logging out user");
-        navigateToPage('index.html');
-      }).catch((error) => {
-        // An error happened.
-      });
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+    console.log("logging out user");
+    navigateToPage('index.html');
+  }).catch((error) => {
+    // An error happened.
+  });
 }
+
+window.addEventListener('scroll', function () {
+  var navbar = document.querySelector('.navbar');
+  if (window.scrollY > 0) {
+    navbar.classList.add('shadow-md');
+  } else {
+    navbar.classList.remove('shadow-md');
+  }
+});
