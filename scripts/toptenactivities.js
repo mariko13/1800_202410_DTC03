fdd4e1883e6d1bbc822067f677a41d7853c9da64
HWEEDONG YOO <yoojon675@gmail.com>
2024-04-04 03:16:10 -0700

    Add click events to activity cards

--- scripts/toptenactivities.js
+++ scripts/toptenactivities.js
@@ -1,5 +1,13 @@
 function goBack() {
-    navigateToPage('home.html');
+    if (document.referrer) {
+        window.location.href = document.referrer;
+    } else {
+        navigateToPage('home.html');
+    }
+}
+
+function navigateToPage(pageUrl) {
+    window.location.href = pageUrl;
 }
 
 function fillTopTenContainer() {
@@ -31,45 +39,47 @@ function fillTopTenContainer() {
             activityRankings.sort((a, b) => b.averageStars - a.averageStars);
             activityRankings.forEach((activity, index) => {
                 console.log(`${index + 1}. Activity: ${activity.activityID}, Average Stars: ${activity.averageStars}`);
-                let activityClass = '';
+                let topTenActivities = '';
                 if (index === 0) {
-                    topTenActivities = `
-                    <article class="flex flex-col place-items-center my-2 text-2xl font-bold bg-[#E22866] rounded-lg py-2">
+                    topTenActivities += `
+                    <a id="rankedActivity-${index}" class="rankedActivity flex flex-col place-items-center my-2 text-2xl font-bold bg-[#E22866] rounded-lg py-2" onclick="localStorage.setItem('selectedActivity', '${activity.activityID}'); window.location.href='activitydetails.html';">
                         <div class="pb-2 text-6xl"><i class="fa-solid fa-crown text-[#F4C015]"></i></div>
-                        <div class="bg-[#F8D04F] py-2 w-full"><img src="./images/${activity.activityID}.svg" class='activity-image w-80 mx-auto'> </div>
+                        <div class="bg-[#F8D04F] py-2 w-full"><img src="./images/${activity.activityID}.svg" class='activity-image w-80 mx-auto'></div>
                         <div class="">${activity.activityID}</div>
                         <div class=""><i class="fa-solid fa-star text-[#F4C015]"></i>   ${activity.averageStars}</div>
-                    </article>
+                    </a>
                 `;
                 } else if (index === 1) {
-                    topTenActivities = `
-                    <article class="flex flex-col place-items-center my-2 text-2xl font-bold bg-[#AE296E] rounded-lg py-2">
+                    topTenActivities += `
+                    <a id="rankedActivity-${index}" class="rankedActivity flex flex-col place-items-center my-2 text-2xl font-bold bg-[#AE296E] rounded-lg py-2" onclick="localStorage.setItem('selectedActivity', '${activity.activityID}'); window.location.href='activitydetails.html';">
                         <div class="pb-2 text-[#F4C015] text-4xl">${index + 1}nd</div>
                         <div class="bg-[#F8D04F] py-2 w-full"><img src="./images/${activity.activityID}.svg" class='activity-image w-64 mx-auto'> </div>
                         <div class="">${activity.activityID}</div>
                         <div class=""><i class="fa-solid fa-star text-[#F4C015]"></i>   ${activity.averageStars}</div>
-                    </article>
+                    </a>
                 `;
                 } else if (index === 2) {
-                    topTenActivities = `
-                    <article class="flex flex-col place-items-center my-2 text-xl font-bold bg-[#3D3264] rounded-lg py-2">
+                    topTenActivities += `
+                    <a id="rankedActivity-${index}" class="rankedActivity flex flex-col place-items-center my-2 text-xl font-bold bg-[#3D3264] rounded-lg py-2" onclick="localStorage.setItem('selectedActivity', '${activity.activityID}'); window.location.href='activitydetails.html';">
                         <div class="pb-2 text-[#F4C015] text-2xl">${index + 1}rd</div>
                         <div class="bg-[#F8D04F] py-2 w-full"><img src="./images/${activity.activityID}.svg" class='activity-image w-56 mx-auto'> </div>
                         <div class="">${activity.activityID}</div>
                         <div class=""><i class="fa-solid fa-star text-[#F4C015]"></i>   ${activity.averageStars}</div>
-                    </article>
+                    </a>
                 `;
                 } else {
-                    topTenActivities = `
-                    <article class="flex flex-col place-items-center my-2 text-xl font-bold bg-[#003952] rounded-lg py-2">
+                    topTenActivities += `
+                    <a id="rankedActivity-${index}" class="rankedActivity flex flex-col place-items-center my-2 text-xl font-bold bg-[#003952] rounded-lg py-2" onclick="localStorage.setItem('selectedActivity', '${activity.activityID}'); window.location.href='activitydetails.html';">
                         <div class="pb-2 text-[#F8D04F] text-2xl">${index + 1}</div>
                         <div class="bg-[#F8D04F] py-2 w-full"><img src="./images/${activity.activityID}.svg" class='activity-image w-44 mx-auto'> </div>
                         <div class="">${activity.activityID}</div>
                         <div class=""><i class="fa-solid fa-star text-[#F4C015]"></i>   ${activity.averageStars}</div>
-                    </article>
+                    </a>
                 `;
                 };
                 topTenContainer.innerHTML += topTenActivities;
+
+                const rankedActivity = document.querySelector(`#rankedActivity-${index}`);
             });
         })
         .catch(error => {
@@ -77,3 +87,5 @@ function fillTopTenContainer() {
         });
 }
 fillTopTenContainer();
+
+
